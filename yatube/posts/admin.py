eaa1d98ec0c5322f365ddb7901daf17047ebe272
mfisher1411 <matthewfisher1411@gmail.com>
2023-12-17 16:16:16 +0500

    Add Group model

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Post, Group
+
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    # Перечислим поля, которые должны отображаться в админке
+    # Добавим в начало столбец pk
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+
+    list_editable = ('group',)
+
+    # Добавляем интерфейс для поиска по тексту постов
+    search_fields = ('text',)
+
+    # Добавляем возможность фильтрации по дате
+    list_filter = ('pub_date',)
+
+     # Это свойство сработает для всех колонок: где пусто - там будет эта строка
+    empty_value_display = '-пусто-'
+
+# При регистрацииииииииии модели Post источником конфигурации для нее назначаем
+# класс PostAdmin
+    
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-
+app_name = 'posts'
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('group/<slug:slug>/', views.group_posts),
 ]
